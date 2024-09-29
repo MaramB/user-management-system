b8963c71faaaf654ff01b89e7277895454b04ee4
MaramB <moury.yellow@gmail.com>
2024-09-29 19:26:09 +0300

    add navbar and translation files

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  currentLang = 'en';
+
+  constructor(
+    private router: Router,
+    private translate: TranslateService,
+    private authService: AuthService
+  ) {
+    this.translate.setDefaultLang(this.currentLang);
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  switchLanguage() {
+    this.currentLang = this.currentLang === 'en' ? 'ar' : 'en';
+    this.translate.use(this.currentLang);
+  }
+}
